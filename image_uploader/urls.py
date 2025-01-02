ec1d4c88a00ecff54f4c337560c5159715f1dd70
Ritik sharma <ritik1832004@gmail.com>
2025-01-02 19:07:29 +0530

    media

--- image_uploader/urls.py
+++ image_uploader/urls.py
@@ -20,6 +20,11 @@ from django.contrib import admin
 from django.urls import path
 from app import views 
 
+
+# for media files 
+
+from django.urls import re_path
+from django.views.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('' , views.home, name="home")
